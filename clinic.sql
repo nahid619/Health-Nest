fdcd3b42e8f4b1b77395e59a8f4aada63ba115d6
Nahid <you@example.com>
2024-09-18 23:25:21 +0600

    first commit

---
+++ clinic.sql
@@ -0,0 +1,393 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 28, 2022 at 07:23 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `clinic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `updationDate`) VALUES
+(1, 'admin', '123admin', '28-12-2016 11:42:05 AM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL,
+  `doctorSpecialization` varchar(255) DEFAULT NULL,
+  `doctorId` int(11) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `consultancyFees` int(11) DEFAULT NULL,
+  `appointmentDate` varchar(255) DEFAULT NULL,
+  `appointmentTime` varchar(255) DEFAULT NULL,
+  `postingDate` timestamp NULL DEFAULT current_timestamp(),
+  `userStatus` int(11) DEFAULT NULL,
+  `doctorStatus` int(11) DEFAULT NULL,
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `doctorSpecialization`, `doctorId`, `userId`, `consultancyFees`, `appointmentDate`, `appointmentTime`, `postingDate`, `userStatus`, `doctorStatus`, `updationDate`) VALUES
+(6, 'Anesthesiologists', 10, 8, 700, '2021-12-04', '7:30 AM', '2021-12-04 01:30:12', 1, 1, NULL),
+(7, 'Anesthesiologists', 10, 9, 700, '2021-12-15', '4:30 PM', '2021-12-15 10:22:30', 1, 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+
+CREATE TABLE `doctors` (
+  `id` int(11) NOT NULL,
+  `specilization` varchar(255) DEFAULT NULL,
+  `doctorName` varchar(255) DEFAULT NULL,
+  `address` longtext DEFAULT NULL,
+  `docFees` varchar(255) DEFAULT NULL,
+  `contactno` bigint(11) DEFAULT NULL,
+  `docEmail` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `creationDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctors`
+--
+
+INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `address`, `docFees`, `contactno`, `docEmail`, `password`, `creationDate`, `updationDate`) VALUES
+(10, 'Anesthesiologists', 'Dr. Imran,  MBBS, FCPS', 'Rajshahi Clinic Point', '700', 1500000001, 'drimran@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2021-12-04 01:17:11', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorslog`
+--
+
+CREATE TABLE `doctorslog` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `userip` binary(16) DEFAULT NULL,
+  `loginTime` timestamp NULL DEFAULT current_timestamp(),
+  `logout` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorslog`
+--
+
+INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
+(20, 10, 'drimran@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-04 01:30:52', '04-12-2021 07:03:35 AM', 1),
+(21, 10, 'drimran@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-04 01:34:27', '04-12-2021 07:05:01 AM', 1),
+(22, 10, 'drimran@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-15 10:11:17', NULL, 1),
+(23, 11, 'imran@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-15 10:23:25', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctorspecilization`
+--
+
+CREATE TABLE `doctorspecilization` (
+  `id` int(11) NOT NULL,
+  `specilization` varchar(255) DEFAULT NULL,
+  `creationDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `doctorspecilization`
+--
+
+INSERT INTO `doctorspecilization` (`id`, `specilization`, `creationDate`, `updationDate`) VALUES
+(13, 'Anesthesiologists', '2021-12-04 01:11:02', NULL),
+(14, 'Cardiologists', '2021-12-04 01:11:27', NULL),
+(15, 'Dermatology', '2021-12-04 01:11:50', NULL),
+(16, 'Family Physicians', '2021-12-04 01:12:06', NULL),
+(17, 'Family Medicine', '2021-12-04 01:12:24', NULL),
+(18, 'General Surgery', '2021-12-04 01:12:38', NULL),
+(19, 'Hematology', '2021-12-04 01:13:02', NULL),
+(20, 'Pathologists', '2021-12-04 01:13:33', NULL),
+(21, 'Neurology', '2021-12-04 01:13:53', NULL),
+(22, 'Ophthalmology', '2021-12-04 01:14:12', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcontactus`
+--
+
+CREATE TABLE `tblcontactus` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `contactno` bigint(12) DEFAULT NULL,
+  `message` mediumtext DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
+  `AdminRemark` mediumtext DEFAULT NULL,
+  `LastupdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `IsRead` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblmedicalhistory`
+--
+
+CREATE TABLE `tblmedicalhistory` (
+  `ID` int(10) NOT NULL,
+  `PatientID` int(10) DEFAULT NULL,
+  `BloodPressure` varchar(200) DEFAULT NULL,
+  `BloodSugar` varchar(200) NOT NULL,
+  `Weight` varchar(100) DEFAULT NULL,
+  `Temperature` varchar(200) DEFAULT NULL,
+  `MedicalPres` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpatient`
+--
+
+CREATE TABLE `tblpatient` (
+  `ID` int(10) NOT NULL,
+  `Docid` int(10) DEFAULT NULL,
+  `PatientName` varchar(200) DEFAULT NULL,
+  `PatientContno` bigint(10) DEFAULT NULL,
+  `PatientEmail` varchar(200) DEFAULT NULL,
+  `PatientGender` varchar(50) DEFAULT NULL,
+  `PatientAdd` mediumtext DEFAULT NULL,
+  `PatientAge` int(10) DEFAULT NULL,
+  `PatientMedhis` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp(),
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userlog`
+--
+
+CREATE TABLE `userlog` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `userip` binary(16) DEFAULT NULL,
+  `loginTime` timestamp NULL DEFAULT current_timestamp(),
+  `logout` varchar(255) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `userlog`
+--
+
+INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`, `status`) VALUES
+(24, 8, 'abc@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-04 01:28:49', NULL, 1),
+(25, 9, 'abcd@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-15 10:16:58', NULL, 1),
+(26, 9, 'abcd@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-15 10:21:17', NULL, 1),
+(27, 9, 'abcd@gmail.com', 0x3a3a3100000000000000000000000000, '2021-12-15 10:22:08', NULL, 1),
+(28, NULL, 'nayem@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-28 05:21:28', NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fullName` varchar(255) DEFAULT NULL,
+  `address` longtext DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `regDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
+(8, 'Md Ruhul', 'Rajshahi - Naogaon Hwy, Rajshahi - Naogaon Hwy', 'Rajshahi', 'male', 'abc@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2021-12-04 01:28:25', NULL),
+(9, 'abc', 'Rajshahi - Naogaon Hwy, Rajshahi - Naogaon Hwy', 'Rajshahi', 'male', 'abcd@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '2021-12-15 10:16:42', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctors`
+--
+ALTER TABLE `doctors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctorslog`
+--
+ALTER TABLE `doctorslog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `doctorspecilization`
+--
+ALTER TABLE `doctorspecilization`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcontactus`
+--
+ALTER TABLE `tblcontactus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblmedicalhistory`
+--
+ALTER TABLE `tblmedicalhistory`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpatient`
+--
+ALTER TABLE `tblpatient`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `userlog`
+--
+ALTER TABLE `userlog`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `doctors`
+--
+ALTER TABLE `doctors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `doctorslog`
+--
+ALTER TABLE `doctorslog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `doctorspecilization`
+--
+ALTER TABLE `doctorspecilization`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `tblcontactus`
+--
+ALTER TABLE `tblcontactus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tblmedicalhistory`
+--
+ALTER TABLE `tblmedicalhistory`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblpatient`
+--
+ALTER TABLE `tblpatient`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `userlog`
+--
+ALTER TABLE `userlog`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rajshahi Clinic Point</title>
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+
+    <!-- bootstrap cdn link  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="INDEX/style.css">
+
+</head>
+<body>
+    
+
+<!-- header section starts  -->
+
+<header>
+
+<div class="container bg-info">
+
+    <a href="#" class="logo">Clinic<span> Point</span></a>
+
+    <nav class="nav ">
+        <ul>
+            <li><a href="#home">home</a></li>
+            <li><a href="#about">about</a></li>
+            <li><a href="#facility">facility</a></li>
+            <li><a href="#review">review</a></li>
+            <li><a href="paitent/user-login.php">paitent</a></li>
+            <li><a href="doctor/index.php">doctor</a></li>
+            <li><a href="admin/index.php">admin</a></li>
+            
+        </ul>
+    </nav>
+
+</div>
+
+</header>
+
+<!-- header section ends  -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="container">
+    
+        <div class="row min-vh-100 align-items-center text-center text-md-left">
+    
+            <div class="col-md-6 pr-md-5">
+                <img src="INDEX/images/teams.png" width="100%" alt="">
+            </div>
+    
+            <div class="col-md-6 pl-md-5 content">
+                <h1><span>stay</span> safe, <span>stay</span> healthy.</h1>
+                <h4>“Good health and good sense are two of life’s greatest blessings.”</h4>
+                <a href="#"><button type="button" class="btn btn-info fs-3 ps-2 pe-2 mt-3 ">Learn More</button></a>
+            </div>
+    
+        </div>
+    
+    </div>
+    
+    </section>
+    
+    
+    <!-- home section ends -->
+
+    <!-- about section start  -->
+
+ <section class="about" id="about">
+
+    <div class="container">
+    
+        <div class="row min-vh-100 align-items-center">
+    
+            <div class="col-md-6 content">
+    
+                <div class="box">
+                    <h3> <i class="fas fa-ambulance"></i>our ambulance </h3>
+                    <p>Hello there, we are provide emergency ambulance service very fast. if you want to get our service then call our hotline number. Thank you.</p>
+                </div>
+    
+                <div class="box">
+                    <h3> <i class="fas fa-procedures"></i> emergency controls </h3>
+                    <p>Our emergency services are very active for emergency patient</p>
+                </div>
+    
+                <div class="box">
+                    <h3> <i class="fas fa-stethoscope"></i> free check-ups </h3>
+                    <p>If you don't have any cost for treatment then you can contact us</p>
+                </div>
+    
+            </div>
+    
+            <div class="col-md-6 d-none d-md-block">
+                <img src="INDEX/images/about.png" width="100%" alt="">
+            </div>
+    
+        </div>
+    
+    </div>
+    
+    </section> 
+    
+    <!-- about section ends -->
+
+    <!-- facility section starts  -->
+
+    <section class="facility" id="facility">
+
+        <div class="container">
+
+            <h1 class="heading"><span>our facilities</span></h1>
+
+        <div class="box-container">
+
+        <div class="box" data-aos="zoom-in">
+        <a href="images/img1.jpg" title="our team">
+            <img src="INDEX/images/facility/1.png" alt="">
+        </a>
+        </div>
+
+     <div class="box" data-aos="zoom-in">
+        <a href="images/img2.jpg" title="our lab">
+            <img src="INDEX/images/facility/2.png" alt="">
+        </a>
+     </div>
+
+        <div class="box" data-aos="zoom-in">
+        <a href="images/img3.jpg" title="emergency rooms">
+            <img src="INDEX/images/facility/3.png" alt="">
+        </a>
+     </div>
+
+     <div class="box" data-aos="zoom-in">
+        <a href="images/img4.jpg" title="expert doctors">
+            <img src="INDEX/images/facility/4.png" alt="">
+        </a>
+     </div>
+
+     <div class="box" data-aos="zoom-in">
+        <a href="images/img5.jpg" title="expert doctors">
+            <img src="INDEX/images/facility/5.png" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img6.jpg" title="emergency rooms">
+            <img src="INDEX/images/facility/6.png" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img7.jpg" title="expert doctors">
+            <img src="INDEX/images/facility/7.png" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img8.jpg" title="emergency rooms">
+            <img src="INDEX/images/facility/8.png" alt="">
+        </a>
+    </div>
+
+        <div class="box" data-aos="zoom-in">
+        <a href="images/img9.jpg" title="enough beds">
+            <img src="INDEX/images/facility/9.png" alt="">
+        </a>
+    </div>
+
+</div>
+
+</div>
+
+</section>
+
+<!-- facility section ends -->
+
+<!-- counter section starts  -->
+
+<section class="counter">
+
+    <div class="container">
+
+        <div class="box-container">
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-hospital"></i>
+                <span>80+</span>
+                <h3>Doctor</h3>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-users"></i>
+                <span>100+</span>
+                <h3>staff</h3>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-smile"></i>
+                <span>1200+</span>
+                <h3>happy patients</h3>
+            </div>
+
+            <div class="box" data-aos="fade-up">
+                <i class="fas fa-procedures"></i>
+                <span>130+</span>
+                <h3>bed facility</h3>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- counter section ends -->
+
+<!-- review section starts  -->
+
+<section class="review" id="review">
+
+<div class="container">
+
+<h1 class="heading"><span>public review </span></h1>
+
+<div class="box-container">
+
+    <div class="box" data-aos="fade-right">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur accusantium error numquam dolore atque. Atque totam ad sint ducimus! Maxime!</p>
+        <h3>Mr. Al Noman</h3>
+        <span>dec 5, 2021</span>
+        <img src="INDEX/images/review/man1.png" alt="">
+    </div>
+
+    <div class="box" data-aos="fade-up">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur accusantium error numquam dolore atque. Atque totam ad sint ducimus! Maxime!</p>
+        <h3>Mr. Shahriar Alom</h3>
+        <span>dec 3, 2021</span>
+        <img src="INDEX/images/review/man2.png" alt="">
+    </div>
+
+    <div class="box" data-aos="fade-left">
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur accusantium error numquam dolore atque. Atque totam ad sint ducimus! Maxime!</p>
+        <h3>Mrs. Khadijatul Islam</h3>
+        <span>dec 10, 2021</span>
+        <img src="INDEX/images/review/man3.png" alt="">
+    </div>
+
+</div>
+
+</div>
+
+</section>
+
+<!-- review section ends  -->
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-4" data-aos="fade-right">
+            <a href="#home" class="logo">Clinic<span> Point</span></a>
+                <p> Tenetur nemo porro quasi minima consequuntur dolorum, quas amet in autem id?</p>
+            </div>
+
+            <div class="col-md-4 text-center" data-aos="fade-up">
+                <h3>links</h3>
+                <a href="#home">home</a>
+                <a href="#about">about</a>
+                <a href="#facility">facility</a>
+                <a href="#review">review</a>
+        
+                
+            </div>
+
+            <div class="col-md-4 text-center" data-aos="fade-left">
+                <h3>social contact</h3>
+                <a href="#">facebook</a>
+                <a href="#">twitter</a>
+                <a href="#">instagram</a>
+                <a href="#">youtube</a>
+            </div>
+
+        </div>
+
+    </div>
+
+    <h4 class="credit text-center mx-auto">&copy; 2023 Rajshahi Clinic Point | all rights reserved.</h4>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   
+
+</body>
