0d619b3d7daa107af3e67dabf386bd13bbcd6b15
Nahid <you@example.com>
2024-10-05 18:50:15 +0600

    initial commit

--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rajshahi Clinic Point</title>
+    <title>Rajshahi Health Nest</title>
 
     <!-- font awesome cdn link -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 
     <!-- bootstrap cdn link  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<!-- 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMo8DY/BkIYi7P1C0Qg4XtT3IWgpjbs6WD9L+Ce" crossorigin="anonymous">
+-->
 
 
     <!-- custom css file link  -->
@@ -23,11 +27,11 @@
 
 <header>
 
-<div class="container bg-info">
+<div class="container navbar">
 
-    <a href="#" class="logo">Clinic<span> Point</span></a>
+    <a href="#" class="logo">Health<span> Nest</span></a>
 
-    <nav class="nav ">
+    <nav class="nav">
         <ul>
             <li><a href="#home">home</a></li>
             <li><a href="#about">about</a></li>
@@ -46,6 +50,10 @@
 
 <!-- header section ends  -->
 
+
+
+
+
 <!-- home section starts  -->
 
 <section class="home" id="home">
@@ -60,8 +68,8 @@
     
             <div class="col-md-6 pl-md-5 content">
                 <h1><span>stay</span> safe, <span>stay</span> healthy.</h1>
-                <h4>“Good health and good sense are two of life’s greatest blessings.”</h4>
-                <a href="#"><button type="button" class="btn btn-info fs-3 ps-2 pe-2 mt-3 ">Learn More</button></a>
+                <h4>“Good health is life’s greatest blessings.”</h4>
+                <a href="#"><button type="button" class="btn btn-primary fs-3 ps-2 pe-2 mt-3 ">Learn More</button></a>
             </div>
     
         </div>
@@ -70,8 +78,11 @@
     
     </section>
     
-    
     <!-- home section ends -->
+
+
+
+
 
     <!-- about section start  -->
 
@@ -111,6 +122,11 @@
     </section> 
     
     <!-- about section ends -->
+
+
+
+
+
 
     <!-- facility section starts  -->
 
@@ -170,11 +186,44 @@
         </a>
     </div>
 
-        <div class="box" data-aos="zoom-in">
+    <div class="box" data-aos="zoom-in">
         <a href="images/img9.jpg" title="enough beds">
             <img src="INDEX/images/facility/9.png" alt="">
         </a>
     </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img9.jpg" title="enough beds">
+            <img src="INDEX/images/facility/10.png" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img9.jpg" title="enough beds">
+            <img src="INDEX/images/facility/11.png" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img9.jpg" title="enough beds">
+            <img src="INDEX/images/facility/12.png" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img9.jpg" title="enough beds">
+            <img src="INDEX/images/facility/13.png" alt="">
+        </a>
+    </div>
+
+    <div class="box" data-aos="zoom-in">
+        <a href="images/img9.jpg" title="enough beds">
+            <img src="INDEX/images/facility/14.png" alt="">
+        </a>
+    </div>
+
+
+
 
 </div>
 
@@ -194,25 +243,25 @@
 
             <div class="box" data-aos="fade-up">
                 <i class="fas fa-hospital"></i>
-                <span>80+</span>
+                <span>90+</span>
                 <h3>Doctor</h3>
             </div>
 
             <div class="box" data-aos="fade-up">
                 <i class="fas fa-users"></i>
-                <span>100+</span>
+                <span>150+</span>
                 <h3>staff</h3>
             </div>
 
             <div class="box" data-aos="fade-up">
                 <i class="fas fa-smile"></i>
-                <span>1200+</span>
+                <span>2000+</span>
                 <h3>happy patients</h3>
             </div>
 
             <div class="box" data-aos="fade-up">
                 <i class="fas fa-procedures"></i>
-                <span>130+</span>
+                <span>150+</span>
                 <h3>bed facility</h3>
             </div>
 
@@ -236,23 +285,30 @@
 
     <div class="box" data-aos="fade-right">
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur accusantium error numquam dolore atque. Atque totam ad sint ducimus! Maxime!</p>
-        <h3>Mr. Al Noman</h3>
-        <span>dec 5, 2021</span>
+        <h3>Mr. Al Maqsud</h3>
+        <span>Jan 31, 2023</span>
         <img src="INDEX/images/review/man1.png" alt="">
+    </div>
+
+    <div class="box" data-aos="fade-up" >
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur accusantium error numquam dolore atque. Atque totam ad sint ducimus! Maxime!</p>
+        <h3>Mr. Nahid Hasan</h3>
+        <span>Jul 22, 2023</span>
+        <img src="INDEX/images/review/man2.png" alt="">
     </div>
 
     <div class="box" data-aos="fade-up">
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur accusantium error numquam dolore atque. Atque totam ad sint ducimus! Maxime!</p>
-        <h3>Mr. Shahriar Alom</h3>
-        <span>dec 3, 2021</span>
-        <img src="INDEX/images/review/man2.png" alt="">
+        <h3>Mrs. Sanzida  Jui</h3>
+        <span>Jul 27, 2023</span>
+        <img src="INDEX/images/review/man3.png" alt="">
     </div>
 
     <div class="box" data-aos="fade-left">
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur accusantium error numquam dolore atque. Atque totam ad sint ducimus! Maxime!</p>
-        <h3>Mrs. Khadijatul Islam</h3>
-        <span>dec 10, 2021</span>
-        <img src="INDEX/images/review/man3.png" alt="">
+        <h3>Mr. Shafin Alam</h3>
+        <span>Feb 14, 2023</span>
+        <img src="INDEX/images/review/man4.png" alt="">
     </div>
 
 </div>
@@ -273,7 +329,7 @@
         <div class="row">
 
             <div class="col-md-4" data-aos="fade-right">
-            <a href="#home" class="logo">Clinic<span> Point</span></a>
+            <a href="#home" class="logo">Health<span> Nest</span></a>
                 <p> Tenetur nemo porro quasi minima consequuntur dolorum, quas amet in autem id?</p>
             </div>
 
@@ -299,7 +355,7 @@
 
     </div>
 
-    <h4 class="credit text-center mx-auto">&copy; 2023 Rajshahi Clinic Point | all rights reserved.</h4>
+    <h4 class="credit text-center mx-auto">&copy; 2023 Rajshahi Health Nest | all rights reserved.</h4>
 
 </section>
 
